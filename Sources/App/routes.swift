8ee87a1bc36e40dd436480d14f2e2a58328eeee9
Raphael Torquato <raphael.martini@live.com>
2023-05-20 08:07:54 -0300

    Updating Movie
    Updating Movie

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -16,7 +16,6 @@ func routes(_ app: Application) throws {
     }
     
     /* Get movie by id /movies/UUID */
-        
     app.get("movies", ":id") { req async throws in
         guard let movie = try await Movie.find(req.parameters.get("id"), on: req.db) else {
             throw Abort(.badRequest)
@@ -24,11 +23,25 @@ func routes(_ app: Application) throws {
         return movie
     }
     
+    /* Delete a movie*/
     app.delete("movies", ":id") { req async throws in
         guard let movie = try await Movie.find(req.parameters.get("id"), on: req.db) else {
             throw Abort(.badRequest)
         }
         try await movie.delete(on: req.db)
+        return movie
+    }
+    
+    /* Update a movie */
+    app.put("movies") { req async throws in
+        let editedMovie = try req.content.decode(Movie.self)
+        /* Movie in the db we want to update */
+        guard let movie = try await Movie.find(editedMovie.id, on: req.db) else {
+            throw Abort(.badRequest)
+        }
+        
+        movie.title = editedMovie.title
+        try await movie.update(on: req.db)
         return movie
     }
 }
