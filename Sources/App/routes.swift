39f59cf53ddebc8018320d7ae7dfcd88cb91d094
Raphael Torquato <raphael.martini@live.com>
2023-05-20 07:52:48 -0300

    Get All Movies & Get Movie By Id
    Get All Movies & Get Movie By Id

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,4 +7,20 @@ func routes(_ app: Application) throws {
         try await movie.save(on: req.db)
         return movie
     }
+    
+    /* Get all movies*/
+    app.get("movies") { req async throws in
+        let movies = try await Movie.query(on: req.db)
+            .all()
+        return movies
+    }
+    
+    /* Get movie by id /movies/UUID */
+        
+    app.get("movies", ":id") { req async throws in
+        guard let movie = try await Movie.find(req.parameters.get("id"), on: req.db) else {
+            throw Abort(.badRequest)
+        }
+        return movie
+    }
 }
